c6cfae534015b5d64bede504d04867b252f3ab00
SrikumarMuralidharan <smuralid@um.edu>
2020-10-31 19:09:41 -0400

    Revert "Initial commit for RWA4. Changes to CMake and XML files."
    This reverts commit 700f585674146c64d87f724dfd3faaec2c71418c.

--- rwa4_group_2/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/srikumar/ariac_ws/src/rwa3_group2
-# Build directory: /home/srikumar/ariac_ws/src/rwa3_group2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
